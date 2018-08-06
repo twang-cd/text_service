4b9bfe6ced0602bb3f37f2bd8a1122b07b1c11cc
Tommy Wang <twang@august8.net>
2018-08-06 07:26:32 +0000

    add case_insensitive to swagger docs; use json boolean as parameter value

--- poc1/alphacheck/views.py
+++ poc1/alphacheck/views.py
@@ -1,8 +1,13 @@
 '''
 views for alphacheck api
 '''
+import json
+
 from django.http import Http404
+import coreapi
+import coreschema
 from rest_framework.response import Response
+from rest_framework.schemas import AutoSchema
 from rest_framework.views import APIView
 
 from .utils import has_all_alphabet
@@ -12,15 +17,30 @@ class CheckAlphabet(APIView):
     '''
     simple APIView for alphacheck api
     '''
+    schema = AutoSchema(manual_fields=[
+        coreapi.Field(
+            'case_insensitive',
+            required=False,
+            location='query',
+            schema=coreschema.Boolean(),
+        ),
+    ])
     # pylint: disable=redefined-builtin, unused-argument
     def get(self, request, query, format=None):
         '''
         HTTP GET method that checks whether query contains all letters of the alphabet
         '''
         try:
+            case_insensitive = json.loads(
+                request.query_params.get('case_insensitive', 'false')
+            )
+        except json.JSONDecodeError:
+            case_insensitive = False
+
+        try:
             response = has_all_alphabet(
                 query,
-                case_insensitive=request.query_params.get('case_insensitive', False),
+                case_insensitive=case_insensitive,
                 version=request.version
             )
         except KeyError:
